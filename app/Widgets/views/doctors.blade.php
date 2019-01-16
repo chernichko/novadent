9dea81b36c3e1b0b4d7ae7507e11694516dbc243
MaryBo <marykupri@gmail.com>
2019-01-17 00:02:37 +0400

    вывод виджета докторов на странице услуг

--- app/Widgets/views/doctors.blade.php
+++ app/Widgets/views/doctors.blade.php
@@ -3,7 +3,11 @@
     <h2>Наши врачи</h2>
     <div class="row">
 	@foreach($doctors as $doctor)
-        <div class="col-xs-12 col-lg-4">
+        @if(count($doctors)==2)
+            <div class="col-xs-12 col-lg-6">
+        @else
+            <div class="col-xs-12 col-lg-4">
+        @endif
             <div class="widget-doctor">
                 <div class="widget-doctor_image">
                     <img src="{{URL::asset('/storage/files/doctors/'.$doctor['image'])}}" class="w-100">
