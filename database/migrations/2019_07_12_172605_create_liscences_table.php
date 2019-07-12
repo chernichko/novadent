cf5b807f698cac579409e1e2b606160acef9132d
Maria Boytsova <marykupri@gmail.com>
2019-07-12 22:06:36 +0400

    миграции

---
+++ database/migrations/2019_07_12_172605_create_liscences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLiscencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('liscences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('path',255);
+            $table->string('alt',25)->default(null)->nullable();
+            $table->string('title',255)->default(null)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('liscences');
+    }
+}
