cbba2cce0b139a54bfe20ced8917456662c86649
Maria Bo <mariaboytsova@MacBook-Pro-Maria.local>
2021-12-17 14:17:16 +0300

    правка страницы

--- app/TextPages.php
+++ app/TextPages.php
@@ -7,24 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class TextPages extends Model
 {
     protected $table = 'text_pages';
+    protected $fillable = ['name', 'code', 'description', 'updated_at'];
 
-    protected $fillable = ['name','code','description','updated_at'];
-
-    protected $hidden = ['created_at'];
-
-    public static function savePage($data){
-
-        if(isset($data['id'])){
-            $page  = TextPages::findOrFail($data['id']);
-        }else{
-            $page  = new TextPages;
+    public static function savePage($data)
+    {
+        if (isset($data['id'])) {
+            $page = TextPages::findOrFail($data['id']);
+        } else {
+            $page = new TextPages;
         }
 
         $page->name = $data['name'];
         $page->code = $data['code'];
         $page->description = $data['description'];
-        $page->active = isset($data['active']) ? 1 : 0 ;
-
+        $page->active = isset($data['active']) ? 1 : 0;
         $page->save();
     }
 }
